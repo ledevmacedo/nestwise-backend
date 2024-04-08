420b52956c483d2f1d50777163395e0e6256238e
Leandro Macedo <leandro.smacedo@Live.com>
2024-04-08 16:34:17 +0100

    feat: organize middleware

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+
+
 const app = express();
 
 // models
 const User = require("./models/User");
+//middleware
+const checkToken = require('./middleware/checkToken'); 
+
 
 // Config JSON response
 app.use(express.json());
@@ -30,23 +35,6 @@ app.get("/user/:id", checkToken, async (req, res) => {
 
     res.status(200).json({ user });
 });
-
-function checkToken(req, res, next) {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) return res.status(401).json({ msg: "Acesso negado!" });
-
-    try {
-        const secret = process.env.SECRET;
-
-        jwt.verify(token, secret);
-
-        next();
-    } catch (err) {
-        res.status(400).json({ msg: "O Token é inválido!" });
-    }
-}
 
 app.post("/auth/register", async (req, res) => {
     const { name, email, password, confirmpassword } = req.body;
